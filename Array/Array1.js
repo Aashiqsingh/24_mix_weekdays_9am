2d89fa926f01c3df1321b36c9ce53c04b1284b08
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-18 10:06:55 +0530

    push , pop

---
+++ Array/Array1.js
@@ -0,0 +1,55 @@
+var user = ["rahul","daniel","dhiraj","samir","roshan","akash"];
+
+// Array index start with 0 
+
+
+// 
+// user.unshift("smit")
+// console.log(user);
+
+// user.shift();
+// console.log(user);
+
+
+
+
+
+
+
+
+
+
+// console.log(user);
+
+
+// user.pop()
+// console.log(user);
+
+
+
+// var x = user.push("smit")
+// var x = user.push("smit","kardam")
+
+
+// console.log(user);
+
+// console.log(x);
+
+
+
+
+
+
+// console.log(user[0])
+// console.log(user[1]);
+// console.log(user.length);
+
+// for(let i=0; i<user.length;i++){
+
+//     console.log(user[i]);
+    
+// }
+
+// Array method 
+
+
---
+++ Array/Array2.js
@@ -0,0 +1,69 @@
+var user = ["rahul","daniel","dhiraj","samir","roshan","akash"];
+
+
+// var x = user.slice(4)
+// var x = user.slice(2,4)
+// var x = user.slice(-3)
+// var x = user.slice(-3,-1)
+
+
+
+// console.log(x);
+
+// var x = user.sort()
+// console.log(x);
+
+// var x = user.splice(3,0,"kardam")
+// var x = user.splice(1,2,"kardam")
+// var x = user.splice(1,2,"kardam","praharsh","harsh")
+
+
+// console.log(user);
+
+
+
+
+
+// var x = user.reverse()
+// console.log(x);
+
+
+
+// var x = user.join(" ")
+// console.log(x);
+
+
+
+
+// var x = user.indexOf("dhiraj")
+// console.log(x);
+
+
+
+// var x = user.includes("samir")
+// console.log(x);
+
+
+
+
+// var arr = [12,13,14,41,14,11]
+
+// var result = user + arr
+// console.log(result);
+
+
+
+
+// var arr2 = [true,false,true]
+
+
+
+
+// var x = user.concat(arr,arr2)
+// console.log(x);
+
+
+var str = "Royal";
+
+var x = str.split("").reverse().join("")
+console.log(x);
