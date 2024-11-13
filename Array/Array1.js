0c963f42d6a88d95d2b62d00773aa1299322de96
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-13 09:56:03 +0530

    array

--- Array/Array1.js
+++ Array/Array1.js
@@ -1,55 +1,69 @@
-var user = ["rahul","daniel","dhiraj","samir","roshan","akash"];
+// array : hetrogenius 
 
-// Array index start with 0 
+// var arr = ["ram",23,3.4,true]
 
+// console.log(student);
+// console.log(student[0]);
 
-// 
-// user.unshift("smit")
-// console.log(user);
-
-// user.shift();
-// console.log(user);
+// console.log(student.length);
 
 
 
+// for(let i=0;i<student.length;i++){
 
-
-
-
-
-
-
-// console.log(user);
-
-
-// user.pop()
-// console.log(user);
-
-
-
-// var x = user.push("smit")
-// var x = user.push("smit","kardam")
-
-
-// console.log(user);
-
-// console.log(x);
-
-
-
-
-
-
-// console.log(user[0])
-// console.log(user[1]);
-// console.log(user.length);
-
-// for(let i=0; i<user.length;i++){
-
-//     console.log(user[i]);
+//     console.log(student[i].toUpperCase());
     
 // }
 
-// Array method 
+// var x = student.push("Hiya")
+// console.log(x);
+
+// student.push("hiya","hetvi","kardam")
+
+// console.log(student);
+
+// var x = student.pop()
+// console.log(x);
+
+// console.log(student);
+
+var student = ["raj","karan","varun","diya","preksha","akansha","makansha"]
+
+var arr = [12,45,67,86,34]
+
+// student.unshift("rahul")
+// console.log(student);
+
+// student.shift()
+// console.log(student);
+
+// var x = student.concat(arr)
+// var x = arr.concat(student)
+
+// console.log(x);
+
+// var x = student.includes("zankhan")
+// console.log(x);
+
+// var x = student.indexOf("akansha")
+// console.log(x);
+
+console.log(student);
+
+// var x = student.join()
+// var x = student.join("-")
+
+// console.log(x);
+
+// var x = student.reverse()
+// console.log(x);
+
+var str = "Royal"
+
+var x = str.split("").reverse().join("")
+console.log(x);
+
+
+
 
 
---
+++ Array/Array3.js
@@ -0,0 +1,32 @@
+var lang = ["hindi","english","norwegian","norwegian","gujarati","marathi"];
+
+
+// var flag = true;
+// for(let i=0;i<lang.length;i++){
+
+//     if(lang[i].length < 6){
+//         flag = false;
+//         break;
+//     }
+
+
+// }
+
+// console.log(flag);
+
+// var x = lang.some((lang)=>{
+//     return lang.length < 5;
+// })
+
+// console.log(x);
+
+// var x = lang.some((lang)=>{
+//     return lang.startsWith("h")
+// })
+// console.log(x);
+
+
+// var x = lang.every((lang)=>{
+//     return lang.startsWith("h")
+// })
+// console.log(x);
