7252daa4ed97e8882ba47bd9860c53765fbbab41
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-01-24 11:49:01 +0530

    .

--- Event/onMouseMove.js
+++ Event/onMouseMove.js
@@ -1,8 +1,19 @@
 var a = 1;
+
+var colors = ["red", "green", "blue", "black","pink","orange","pinkgreen","yellow"]
+
+// console.log(colors[0]);
+
 function change(){
 
-    console.log(a++);
+    // console.log(a++);
     // console.log("kardam");
+
+    var mydiv = document.getElementById("mydiv");
     
+    var randomIndex = Math.floor(Math.random() * colors.length)
+    
+    // console.log(randomIndex);
+    mydiv.style.backgroundColor = colors[randomIndex]
     
 }
