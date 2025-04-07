6ecc3add6f7639f6a60956b1a2fdb487e0c66f9c
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-04-07 11:30:50 +0530

    sync

---
+++ Promise/sync2.js
@@ -0,0 +1,46 @@
+const getData = ()=>{
+
+    const promise = new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            resolve({
+                message:"Data fetched successfully..."
+            })
+        },3000)
+    })
+
+    // console.log(promise);
+
+    // promise.then((data)=>{
+    //     console.log(data);
+        
+    // })
+    // promise.catch((err)=>{
+    //     console.error(err);
+    // })
+
+    // promise.then((data)=>{
+    //     console.log(data);
+    // }).catch((err)=>{
+    //     console.error(err);
+    // })
+
+    return promise;
+    
+
+}
+
+// var ans = getData();
+// ans.then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.error(err);
+// })
+
+
+getData().then((data)=>{
+    console.log(data);
+}).catch((err)=>{
+    console.log(err);
+    
+})
